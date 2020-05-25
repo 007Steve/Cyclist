a303497e553b7ec85f8eb855d6e2de1861785575
007steve <stephen.plummer1.sp@gmail.com>
2020-05-25 15:45:26 -0400

    added validation to trainer model

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,5 +1,6 @@
 class Trainer < ApplicationRecord
     has_many :rides
     has_many :users, through: :rides
-
+    validates :name, presence: true
+    validates :bio, length: { maximum: 500 }
 end
