8b55edcb703d525bba687eeda3c9bee5092d1402
007steve <stephen.plummer1.sp@gmail.com>
2020-05-25 15:37:31 -0400

    added relationship between models

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,2 +1,5 @@
 class Trainer < ApplicationRecord
+    has_many :rides
+    has_many :users, through: :rides
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :rides
+    has_many :users, through: :rides
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
 end
