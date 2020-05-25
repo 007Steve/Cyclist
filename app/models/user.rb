8daf310b2db034d98b7948b608d247dcc1239651
007steve <stephen.plummer1.sp@gmail.com>
2020-05-25 15:47:11 -0400

    added validation to user  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :rides
     has_many :users, through: :rides
     has_secure_password
+    validates :email, presence: true
     validates :username, presence: true, uniqueness: true
 end
